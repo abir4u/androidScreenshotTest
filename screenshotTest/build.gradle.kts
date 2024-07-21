2c7c53acfb05788cd59c03f4fb66744bfca95cda
abir4u <abirpal90@gmail.com>
2024-07-21 17:03:53 +1200

    Issue related to building screenshot test fixed in this PR

--- screenshotTest/build.gradle.kts
+++ screenshotTest/build.gradle.kts
@@ -32,7 +32,22 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+}
+
+// TODO Remove when https://github.com/google/guava/issues/6567 is fixed.
+// See also: https://github.com/google/guava/issues/6801.
+dependencies.constraints {
+    testImplementation("com.google.guava:guava") {
+        attributes {
+            attribute(
+                TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
+                objects.named(TargetJvmEnvironment::class.java, TargetJvmEnvironment.STANDARD_JVM)
+            )
+        }
+        because("Paparazzi's layoutlib and sdk-common depend on Guava's -jre published variant." +
+                "See https://github.com/cashapp/paparazzi/issues/906.")
     }
 }
 
@@ -40,9 +55,12 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.activity.compose)
     implementation(libs.material)
     testImplementation(libs.junit)
+    testImplementation(libs.guava)
     androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(project(":app"))
 }
